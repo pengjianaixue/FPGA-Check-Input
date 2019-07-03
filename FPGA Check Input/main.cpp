ce174b836a7df9f03d529f1565de99b126651afc
jian peng <jian.peng@ericsson.com>
2019-07-03 11:10:50 +0800

    Change the Windows Title

--- FPGA Check Input/main.cpp
+++ FPGA Check Input/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 	QApplication a(argc, argv);
 	QApplication::setStyle(QStyleFactory::create("Fusion"));
 	MainWindow w;
-	w.setWindowTitle("FPGA Check Input");
+	w.setWindowTitle("FPGA Self-Check Input Tool");
 	w.setMinimumSize(200, 150); 
 	w.show();
 	return a.exec();
